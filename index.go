8c06e7d8659190055f68e4dae488122e10adcaa2
pradityamsook <praditcry@gmail.com>
2017-08-11 23:06:02 +0700

    Add files via upload

---
+++ index.go
@@ -0,0 +1,25 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+)
+
+func main(){
+	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Many boy")
+	})
+
+	http.HandleFunc("/product",product)
+	http.HandleFunc("/user", user)
+  
+	http.ListenAndServe(":8080",nil)
+}
+
+func product(w http.ResponseWriter, t *http.Request){
+	fmt.Fprintf(w, "Product request")
+}
+
+func user(hr http.ResponseWriter, t *http.Request){
+	fmt.Fprintf(hr, "user request")
+}
