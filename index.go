ab316649841225b01fd5c1c8d981b156d6d290a1
pradityamsook <praditcry@gmail.com>
2017-08-13 16:14:13 +0700

    golang web app

--- index.go
+++ index.go
@@ -6,15 +6,30 @@ import(
 )
 
 func main(){
+
+	userDB := map[string]int{
+		"pradit" : 21,
+		"golang" : 25,
+		"java"   : 10,
+		"python" : 100,
+	}
+
 	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, "Many boy")
 	})
 
-	http.HandleFunc("/product",product)
-	http.HandleFunc("/user", user)
+	http.HandleFunc("/user/",func(w http.ResponseWriter, r *http.Request){
+		name := r.URL.Path[len("/user/"):]
+		age := userDB[name]
+		fmt.Fprintf(w, "I am %s %d years old",name, age)
+	})
+
+	
   
 	http.ListenAndServe(":8080",nil)
+
 }
+
 
 func product(w http.ResponseWriter, t *http.Request){
 	fmt.Fprintf(w, "Product request")
