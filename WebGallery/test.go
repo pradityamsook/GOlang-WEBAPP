a3d8608cfdb58b7bae8a9e9bebe1f6d6a388f031
Leon <leon@localhost.localdomain>
2018-03-17 01:33:29 +0700

    update website gallery March 17 2018

---
+++ WebGallery/test.go
@@ -0,0 +1,23 @@
+package main 
+
+import (
+	"log"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func main() {
+	session, err := mgo.Dial("127.0.0.1:27017")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	
+	defer session.Close()
+	collection := session.DB("test").C("go")
+	err = collection.Insert(bson.M{"name": "Pradit"})
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
