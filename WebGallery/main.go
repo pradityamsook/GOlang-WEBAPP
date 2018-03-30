b5bfa4bfd7cbb6816b1f5f10d857a5f8a93b51bf
Leon <leon@localhost.localdomain>
2018-03-30 15:15:14 +0700

    update golang sqlite web application picture

--- WebGallery/main.go
+++ WebGallery/main.go
@@ -4,8 +4,10 @@ import (
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
 	"html/template"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 )
@@ -18,6 +20,7 @@ type Page struct {
 var (
 	tmplView      = template.Must(template.New("test").ParseFiles("base.html", "test.html", "index.html"))
 	tmplEdit      = template.Must(template.New("edit").ParseFiles("base.html", "edit.html", "index.html"))
+	tmplUpload    = template.Must(template.New("upload").ParseFiles("base.html", "upload.html", "index.html"))
 	db, _         = sql.Open("sqlite3", "cache/web.db")
 	creatDatabase = "create table if not exists pages (title text, body blob, timestamp text)"
 )
@@ -27,6 +30,7 @@ func (p *Page) saveCache() error {
 	f := "cache/" + p.Title + ".txt"
 	db.Exec(creatDatabase)
 	tx, _ := db.Begin()
+	stmt, _ := tx.Prepare("insert into pages (title, body, timestamp) values (?, ?, ?)")
 	_, err := stmt.Exec(p.Title, p.Body, timestamp)
 	tx.Commit()
 	ioutil.WriteFile(f, p.Body, 0600)
@@ -43,9 +47,30 @@ func load(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func loadSource(title string) (*Page, error) {
+	var name string
+	var body []byte
+	q, err := db.Query("select title, body from pages where title = '" + title + "' order by timestamp desc limit 1")
+	if err != nil {
+		return nil, err
+	}
+	for q.Next() {
+		q.Scan(&name, &body)
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
 func view(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/test/"):]
-	p, _ := load(title)
+	p, err := loadSource(title)
+	if err != nil {
+		p, _ = load(title)
+	}
+
+	if p.Title == "" {
+		p, _ = load(title)
+	}
+	//p, _ := load(title)
 	tmplView.ExecuteTemplate(w, "base", p)
 	//t, _ := template.ParseFiles("test.html")
 	//t.Execute(w, p)
@@ -55,24 +80,72 @@ func save(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/save/"):]
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
-	p.save()
+	p.saveCache()
 	http.Redirect(w, r, "/test/"+title, http.StatusFound)
 }
 
 func edit(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/edit/"):]
-	p, _ := load(title)
+	p, err := loadSource(title)
+	if err != nil {
+		p, _ = load(title)
+	}
+
+	if p.Title == "" {
+		p, _ = load(title)
+	}
+	//p, _ := load(title)
 	tmplEdit.ExecuteTemplate(w, "base", p)
 	//t, _ := template.ParseFiles("edit.html")
 	//t.Execute(w, p)
 }
 
+func upload(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		title := "Upload"
+		p := &Page{Title: title}
+		tmplUpload.ExecuteTemplate(w, "base", p)
+	case "POST":
+		err := r.ParseMultipartForm(100000)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		m := r.MultipartForm
+		files := m.File["myfiles"]
+
+		for i := range files {
+			file, err := files[i].Open()
+			defer file.Close()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			f, err := os.Create("./files/" + files[i].Filename)
+			defer f.Close()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			if _, err := io.Copy(f, file); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			http.Redirect(w, r, "/files/"+files[i].Filename, http.StatusFound)
+		}
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir("files"))))
 	p := &Page{Title: "Test", Body: []byte("Glad you come!")}
-	p.save()
+	p.saveCache()
 	http.HandleFunc("/test/", view)
 	http.HandleFunc("/edit/", edit)
 	http.HandleFunc("/save/", save)
+	http.HandleFunc("/upload/", upload)
 	http.ListenAndServe(":8000", nil)
 }
